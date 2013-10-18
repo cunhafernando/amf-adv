64e88fa15d0fc96c4857c0215706873e756e7fab
cunhafernando <fernando@trevobr.com>
2013-10-18 10:57:19 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmfAdv::Application.config.secret_token = '165e1b3a3667a659f1580b1dc9e1a31392972d66d539aa1ec2f711396565439ec1feadd29525b9bd0cadbbef82c07a182650d316f680f89fab78569daec88a8c'
