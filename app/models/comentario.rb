96905a79bce2ba93a8d175e36bba230c60f19199
cunhafernando <fernando@trevobr.com>
2013-11-02 09:07:10 -0200

    Revert "Alteraçoes erradas"
    This reverts commit 44b9560ac6245cc4e463bbd839484e9472edba96.

--- app/models/comentario.rb
+++
@@ -1,4 +0,0 @@
-class Comentario < ActiveRecord::Base
-  belongs_to :post
-  attr_accessible :autor, :conteudo
-end
--- app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ActiveRecord::Base
-  has_many :comentarios
-  attr_accessible :conteudo, :titulo, :autor
-  validates :titulo, :presence => true, :length => { :minimum => 10 }
-  validates :conteudo, :presence => true
-end
