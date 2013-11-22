00751f05acbc4dddf28015b1134b080cbdfee2c2
cunhafernando <fernando@trevobr.com>
2013-11-22 13:22:55 -0200

    rodape e webicon

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   
   def index
-    @title = 'AM Farias Advogado - Home'
+    @title = 'Antonio Martins de Farias Advogado - Home'
     @informes = Informe.order("created_at desc").limit(2)
     @clausulas = Clausula.order("created_at desc").limit(2)
     @decisaos = Decisao.order("created_at desc").limit(2)
@@ -9,27 +9,27 @@ class WelcomeController < ApplicationController
   end
   
   def contato
-    @title = 'AM Farias Advogado - Contato'
+    @title = 'Antonio Martins de Farias Advogado - Contato'
   end
   
   def consumidor
-    @title = 'AM Farias advogado - Direito do Consumidor'
+    @title = 'Antonio Martins de Farias advogado - Direito do Consumidor'
   end
   
   def previdenciario
-    @title = 'AM Farias Advogado - Direito Previdenciario'
+    @title = 'Antonio Martins de Farias Advogado - Direito Previdenciario'
   end
 
   def servicos
-    @title = 'AM Farias Advogado - Links Uteis'
+    @title = 'Antonio Martins de Farias Advogado - Links Uteis'
   end
   
   def tributario
-    @title = 'AM Farias Advogado - Direito Tributario'
+    @title = 'Antonio Martins de Farias Advogado - Direito Tributario'
   end
   
   def atuacao
-    @title = 'AM Farias Advogado - Area de Atuacao'
+    @title = 'Antonio Martins de Farias Advogado - Area de Atuacao'
   end
 
 end
